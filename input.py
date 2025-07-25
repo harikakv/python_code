05dc7b536a1a4705a3652d484105e3e86d87737d
harikakv <harikakv059@gmail.com>
2025-07-25 21:54:11 +0530

    input statement

---
+++ input.py
@@ -0,0 +1,35 @@
+
+# print two input numbers
+
+#result for input( ) is always a string
+
+a=int(input("enter number a"))
+
+b=int(input("enter number b") )
+
+print (a+b)
+
+
+#print square sides
+
+x= float(input ("enter the side of square :"))
+
+print(x*x)
+
+
+#print avg
+
+y=float(input("enter first integer:"))
+
+z=float(input("enter second integer:"))
+
+print((y+z) / 2)
+
+
+#print true or flase 
+
+num1= int(input("enter your first number:"))
+
+num2= int(input("enter your second number:"))
+
+print(num1 >= num2)
